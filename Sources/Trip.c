11525048ce2565e5c386b9b55e67700178b6f23b
lookell <61921888+lookell@users.noreply.github.com>
2020-03-16 19:56:52 +1030

    Implement most functions in Trip.c
    Missing:
    * ToggleTV

--- Sources/Trip.c
+++ Sources/Trip.c
@@ -21,24 +21,18 @@ extern	Boolean		tvOn;
 
 void ToggleToaster (SInt16 index)
 {
-	return;
-#if 0
 	dinahs[index].active = !dinahs[index].active;
-#endif
 }
 
 //--------------------------------------------------------------  ToggleMacPlus
 
 void ToggleMacPlus (SInt16 index)
 {
-	return;
-#if 0
 	dinahs[index].active = !dinahs[index].active;
 	if (dinahs[index].active)
 		dinahs[index].timer = 40;
 	else
 		dinahs[index].timer = 10;
-#endif
 }
 
 //--------------------------------------------------------------  ToggleTV
@@ -70,135 +64,97 @@ void ToggleTV (SInt16 index)
 
 void ToggleCoffee (SInt16 index)
 {
-	return;
-#if 0
 	dinahs[index].active = !dinahs[index].active;
 	dinahs[index].timer = 4;
-#endif
 }
 
 //--------------------------------------------------------------  ToggleOutlet
 
 void ToggleOutlet (SInt16 index)
 {
-	return;
-#if 0
 	dinahs[index].active = !dinahs[index].active;
-#endif
 }
 
 //--------------------------------------------------------------  ToggleVCR
 
 void ToggleVCR (SInt16 index)
 {
-	return;
-#if 0
 	dinahs[index].active = !dinahs[index].active;
 	dinahs[index].timer = 4;
-#endif
 }
 
 //--------------------------------------------------------------  ToggleStereo
 
 void ToggleStereos (SInt16 index)
 {
-	return;
-#if 0
 	if (dinahs[index].timer == 0)
 	{
 		dinahs[index].active = !dinahs[index].active;
 		dinahs[index].timer = 4;
 	}
-#endif
 }
 
 //--------------------------------------------------------------  ToggleMicrowave
 
 void ToggleMicrowave (SInt16 index)
 {
-	return;
-#if 0
 	dinahs[index].active = !dinahs[index].active;
 	dinahs[index].timer = 4;
-#endif
 }
 
 //--------------------------------------------------------------  ToggleBalloon
 
 void ToggleBalloon (SInt16 index)
 {
-	return;
-#if 0
 	dinahs[index].active = !dinahs[index].active;
-#endif
 }
 
 //--------------------------------------------------------------  ToggleCopter
 
 void ToggleCopter (SInt16 index)
 {
-	return;
-#if 0
 	dinahs[index].active = !dinahs[index].active;
-#endif
 }
 
 //--------------------------------------------------------------  ToggleDart
 
 void ToggleDart (SInt16 index)
 {
-	return;
-#if 0
 	dinahs[index].active = !dinahs[index].active;
-#endif
 }
 
 //--------------------------------------------------------------  ToggleBall
 
 void ToggleBall (SInt16 index)
 {
-	return;
-#if 0
 	dinahs[index].active = !dinahs[index].active;
-#endif
 }
 
 //--------------------------------------------------------------  ToggleDrip
 
 void ToggleDrip (SInt16 index)
 {
-	return;
-#if 0
 	dinahs[index].active = !dinahs[index].active;
-#endif
 }
 
 //--------------------------------------------------------------  ToggleFish
 
 void ToggleFish (SInt16 index)
 {
-	return;
-#if 0
 	dinahs[index].active = !dinahs[index].active;
-#endif
 }
 
 //--------------------------------------------------------------  TriggerSwitch
 
 void TriggerSwitch (SInt16 who)
 {
-	return;
-#if 0
 	HandleSwitches(&hotSpots[who]);
-#endif
 }
 
 //--------------------------------------------------------------  TriggerToast
 
 void TriggerToast (SInt16 who)
 {
-	return;
-#if 0
 	if (!dinahs[who].moving)
 	{
 		if (dinahs[who].active)
@@ -211,15 +167,12 @@ void TriggerToast (SInt16 who)
 		else
 			dinahs[who].frame = dinahs[who].timer;
 	}
-#endif
 }
 
 //--------------------------------------------------------------  TriggerOutlet
 
 void TriggerOutlet (SInt16 who)
 {
-	return;
-#if 0
 	if (dinahs[who].position == 0)
 	{
 		if (dinahs[who].active)
@@ -231,26 +184,20 @@ void TriggerOutlet (SInt16 who)
 		else
 			dinahs[who].timer = dinahs[who].count;
 	}
-#endif
 }
 
 //--------------------------------------------------------------  TriggerDrip
 
 void TriggerDrip (SInt16 who)
 {
-	return;
-#if 0
 	if ((!dinahs[who].moving) && (dinahs[who].timer > 7))
 		dinahs[who].timer = 7;		// kick off drip
-#endif
 }
 
 //--------------------------------------------------------------  TriggerFish
 
 void TriggerFish (SInt16 who)
 {
-	return;
-#if 0
 	if ((dinahs[who].active) && (!dinahs[who].moving))
 	{
 		dinahs[who].whole = dinahs[who].dest;
@@ -258,18 +205,14 @@ void TriggerFish (SInt16 who)
 		dinahs[who].frame = 4;
 		PlayPrioritySound(kFishOutSound, kFishOutPriority);
 	}
-#endif
 }
 
 //--------------------------------------------------------------  TriggerBalloon
 
 void TriggerBalloon (SInt16 who)
 {
-	return;
-#if 0
 	if (!dinahs[who].moving)
 		dinahs[who].timer = kStartSparkle + 1;
-#endif
 }
 
 
@@ -277,11 +220,8 @@ void TriggerBalloon (SInt16 who)
 
 void TriggerCopter (SInt16 who)
 {
-	return;
-#if 0
 	if (!dinahs[who].moving)
 		dinahs[who].timer = kStartSparkle + 1;
-#endif
 }
 
 
@@ -289,26 +229,20 @@ void TriggerCopter (SInt16 who)
 
 void TriggerDart (SInt16 who)
 {
-	return;
-#if 0
 	if (!dinahs[who].moving)
 		dinahs[who].timer = kStartSparkle + 1;
-#endif
 }
 
 //--------------------------------------------------------------  UpdateOutletsLighting
 
 void UpdateOutletsLighting (SInt16 room, SInt16 nLights)
 {
-	return;
-#if 0
-	short		i;
+	SInt16		i;
 
 	for (i = 0; i < numDynamics; i++)
 	{
 		if ((dinahs[i].type == kOutlet) && (dinahs[i].room == room))
 			dinahs[i].hVel = nLights;
 	}
-#endif
 }
 
