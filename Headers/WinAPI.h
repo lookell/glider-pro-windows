cdfac58f151f8d9170edb37c5f05c3aa1a58e12d
lookell <61921888+lookell@users.noreply.github.com>
2020-03-16 21:00:40 +1030

    Add <windows.h> wrapper header

---
+++ Headers/WinAPI.h
@@ -0,0 +1,19 @@
+// The following definitions ask for the following:
+//
+// * strict type definitions (this is the default, but just to be sure...)
+// * use Unicode for unsuffixed functions
+// * all API definitions available since Windows XP
+
+#define STRICT
+#define UNICODE
+#define WINVER _WIN32_WINNT
+#define _WIN32_WINNT _WIN32_WINNT_WINXP
+#define _WIN32_IE_ _WIN32_IE_WINXP
+
+#include <windows.h>
+
+// The following pseudo-variable gives access to the hInstance
+// of the current module.
+
+EXTERN_C IMAGE_DOS_HEADER __ImageBase;
+#define HINST_THISCOMPONENT ((HINSTANCE)&__ImageBase)
