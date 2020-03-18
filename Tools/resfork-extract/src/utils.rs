f5d57aa58c277479ad5a6927919389e5bf721e34
lookell <61921888+lookell@users.noreply.github.com>
2020-03-19 08:57:21 +1030

    Refactored resfork-extract/src/utils.rs

--- Tools/resfork-extract/src/utils.rs
+++ Tools/resfork-extract/src/utils.rs
@@ -1,23 +1,6 @@
 use std::io;
 use std::io::{Read, Seek, SeekFrom, Write};
 
-pub(crate) trait ReadExt: Read {
-    fn read_be_u8(&mut self) -> io::Result<u8>;
-    fn read_le_u8(&mut self) -> io::Result<u8>;
-    fn read_be_i8(&mut self) -> io::Result<i8>;
-    fn read_le_i8(&mut self) -> io::Result<i8>;
-
-    fn read_be_u16(&mut self) -> io::Result<u16>;
-    fn read_le_u16(&mut self) -> io::Result<u16>;
-    fn read_be_i16(&mut self) -> io::Result<i16>;
-    fn read_le_i16(&mut self) -> io::Result<i16>;
-
-    fn read_be_u32(&mut self) -> io::Result<u32>;
-    fn read_le_u32(&mut self) -> io::Result<u32>;
-    fn read_be_i32(&mut self) -> io::Result<i32>;
-    fn read_le_i32(&mut self) -> io::Result<i32>;
-}
-
 fn read_one_byte(mut reader: impl Read) -> io::Result<[u8; 1]> {
     let mut buf = [0; 1];
     reader.read_exact(&mut buf).map(|_| buf)
@@ -33,7 +16,9 @@ fn read_four_bytes(mut reader: impl Read) -> io::Result<[u8; 4]> {
     reader.read_exact(&mut buf).map(|_| buf)
 }
 
-impl<R: Read> ReadExt for R {
+impl<R: Read> ReadExt for R {}
+
+pub(crate) trait ReadExt: Read {
     fn read_be_u8(&mut self) -> io::Result<u8> {
         read_one_byte(self).map(u8::from_be_bytes)
     }
@@ -83,24 +68,9 @@ impl<R: Read> ReadExt for R {
     }
 }
 
+impl<W: Write> WriteExt for W {}
+
 pub(crate) trait WriteExt: Write {
-    fn write_be_u8(&mut self, v: u8) -> io::Result<()>;
-    fn write_le_u8(&mut self, v: u8) -> io::Result<()>;
-    fn write_be_i8(&mut self, v: i8) -> io::Result<()>;
-    fn write_le_i8(&mut self, v: i8) -> io::Result<()>;
-
-    fn write_be_u16(&mut self, v: u16) -> io::Result<()>;
-    fn write_le_u16(&mut self, v: u16) -> io::Result<()>;
-    fn write_be_i16(&mut self, v: i16) -> io::Result<()>;
-    fn write_le_i16(&mut self, v: i16) -> io::Result<()>;
-
-    fn write_be_u32(&mut self, v: u32) -> io::Result<()>;
-    fn write_le_u32(&mut self, v: u32) -> io::Result<()>;
-    fn write_be_i32(&mut self, v: i32) -> io::Result<()>;
-    fn write_le_i32(&mut self, v: i32) -> io::Result<()>;
-}
-
-impl<W: Write> WriteExt for W {
     fn write_be_u8(&mut self, v: u8) -> io::Result<()> {
         self.write_all(&v.to_be_bytes())
     }
@@ -150,11 +120,9 @@ impl<W: Write> WriteExt for W {
     }
 }
 
-pub(crate) trait SeekExt: Seek {
-    fn align_to(&mut self, align: u64) -> io::Result<u64>;
-}
+impl<S: Seek> SeekExt for S {}
 
-impl<S: Seek> SeekExt for S {
+pub(crate) trait SeekExt: Seek {
     fn align_to(&mut self, align: u64) -> io::Result<u64> {
         let cur_pos = self.seek(SeekFrom::Current(0))?;
         let offset = align - (cur_pos % align);
