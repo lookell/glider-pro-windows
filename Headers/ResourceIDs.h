58e9d984e4197d036ecd7a7e534ea478c5dd4b85
lookell <61921888+lookell@users.noreply.github.com>
2020-04-03 14:26:00 +1030

    Add program menus to resource script

---
+++ Headers/ResourceIDs.h
@@ -0,0 +1,33 @@
+#define IDM_APPLE				128
+#define ID_ABOUT				40101
+
+#define IDM_GAME				129
+#define ID_NEW_GAME				40201
+#define ID_TWO_PLAYER			40202
+#define ID_OPEN_SAVED_GAME		40203
+#define ID_LOAD_HOUSE			40205
+#define ID_QUIT					40207
+
+#define IDM_OPTIONS				130
+#define ID_EDITOR				40301
+#define ID_HIGH_SCORES			40303
+#define ID_PREFS				40304
+#define ID_DEMO					40305
+
+#define IDM_HOUSE				131
+#define ID_NEW_HOUSE			40401
+#define ID_SAVE_HOUSE			40402
+#define ID_HOUSE_INFO			40404
+#define ID_ROOM_INFO			40405
+#define ID_OBJECT_INFO			40406
+#define ID_CUT					40408
+#define ID_COPY					40409
+#define ID_PASTE				40410
+#define ID_CLEAR				40411
+#define ID_DUPLICATE			40412
+#define ID_BRING_FORWARD		40414
+#define ID_SEND_BACK			40415
+#define ID_GO_TO_ROOM			40417
+#define ID_MAP_WINDOW			40419
+#define ID_OBJECT_WINDOW		40420
+#define ID_COORDINATE_WINDOW	40421
