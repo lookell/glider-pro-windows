acbb5ec1df0cd3d8a5d46edb13223bb417ae97ca
lookell <61921888+lookell@users.noreply.github.com>
2020-09-25 16:14:18 +0930

    Don't check for negative dimensions in QInsetRect
    This fixes some rendering bugs with the very thin furniture used in
    the SpacePods house. This house relies on the object drawing code's
    odd behavior when drawing slim furniture, including the fact that
    rectangles with negative width or height happen during the drawing
    process. It expects the negative width and height to happen.

--- Sources/RectUtils.c
+++ Sources/RectUtils.c
@@ -251,25 +251,18 @@ void QUnionSimilarRect (const Rect *rectA, const Rect *rectB, Rect *rectC)
 //--------------------------------------------------------------  QInsetRect
 // Shrink or expand the given rectangle. The rectangle's sides
 // are moved inwards by the given deltas. To expand the rectangle,
-// call this function with negative delta values. If either the width
-// or the height of the resulting rectangle is less than 1, then the
-// rectangle is set to the empty rectangle (0,0,0,0).
+// call this function with negative delta values. No correction is
+// applied if the resulting rectangle has a negative width or height.
 
 void QInsetRect (Rect *r, SInt16 dh, SInt16 dv)
 {
 	if (r == NULL)
 		return;
+
 	r->left += dh;
 	r->top += dv;
 	r->right -= dh;
 	r->bottom -= dv;
-	if (r->left > r->right || r->top > r->bottom)
-	{
-		r->left = 0;
-		r->top = 0;
-		r->right = 0;
-		r->bottom = 0;
-	}
 }
 
 //--------------------------------------------------------------  QPtInRect
